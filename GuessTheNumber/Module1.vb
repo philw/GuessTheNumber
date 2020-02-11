76b675fccb9c8cf413b183be91de97ff52b03318
Phil Wood <phil@pwood.net>
2020-02-11 20:49:24 +0000

    removed Randomize as it is not needed

--- GuessTheNumber/Module1.vb
+++ GuessTheNumber/Module1.vb
@@ -9,7 +9,6 @@
         Dim NumberOfGuesses As Integer = 0
         Dim Rnd As New Random
 
-        Randomize()
         SecretNumber = Rnd.Next(1, MaximumNumber + 1)
 
         Console.WriteLine("The secret number is between 1 and " & MaximumNumber)
